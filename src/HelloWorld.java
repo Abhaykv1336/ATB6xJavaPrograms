ece9c2afd504bdc0195a955233619aa503eef0b8
Abhaykv1336 <abhaykv1336@gmail.com>
2024-04-18 08:22:23 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c=30;
+        String Name = "Abhay";
+        System.out.println(a+b+c+Name); //60Abhay
+        System.out.println(Name+a+b+c); //Abhay102030
+        System.out.print("Hello \n \tAbhay");
+    }
+}
