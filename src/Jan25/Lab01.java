f448c55a2f8193e601f4fa2df762004a99efa8df
Abhaykv1336 <abhaykv1336@gmail.com>
2025-01-05 14:06:33 +0530

    Jan25

---
+++ src/Jan25/Lab01.java
@@ -0,0 +1,10 @@
+package Jan25;
+
+public class Lab01 {
+    public static void main (String [] args){
+        int age_mamitha= 40;
+        int age_pramode=35;
+        boolean result = age_pramode>age_mamitha;
+        System.out.println(result);
+    }
+}
